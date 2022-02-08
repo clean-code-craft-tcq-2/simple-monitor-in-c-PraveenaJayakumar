378dcafec235dc08bbaef637e57024eb7ac4a12b
PraveenaJayakumar <90901057+PraveenaJayakumar@users.noreply.github.com>
2022-02-08 21:15:47 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -2,12 +2,6 @@
 #include <assert.h>
 #include "Parameters.h"
 
-#define MINIMUM_TEMPERATURE    0.0
-#define MAXIMUM_TEMPERATURE    45.0
-#define MINIMUM_SOC    20.0
-#define MAXIMUM_SOC    80.0
-#define MINIMUM_CHARGERATE    0
-#define MAXIMUM_CHARGERATE    0.8
 
 void display(char *stringsToBeDisplayed) {
     if(stringsToBeDisplayed != NULL)
