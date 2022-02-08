7d16c796c0c48b7f7df3af7bef290931a030a82e
PraveenaJayakumar <90901057+PraveenaJayakumar@users.noreply.github.com>
2022-02-08 21:11:06 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -30,7 +30,7 @@ int Is_state_of_charge_not_safe(float soc) {
   return 1;
 }
 
-int Is_state_of_charge_not_safe(float chargeRate) {
+int Is_chargerate_out_of_range(float chargeRate) {
 	
   if(chargeRate > MAXIMUM_CHARGERATE) {
     display("Charge Rate out of range!\n");
@@ -40,7 +40,7 @@ int Is_state_of_charge_not_safe(float chargeRate) {
 }
 int batteryIsOk(float temperature, float soc, float chargeRate) {
   
-  return ( (Is_Temperature_safe(temperature))(Is_state_of_charge_not_safe( soc))(Is_state_of_charge_not_safe( chargeRate)));
+  return ( (Is_Temperature_safe(temperature))(Is_state_of_charge_not_safe( soc))(Is_chargerate_out_of_range( chargeRate)));
 }
 
 int main() {
