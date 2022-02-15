04555016c0ff50f6dc977b6d6076a2cc2a141fc3
PraveenaJayakumar <90901057+PraveenaJayakumar@users.noreply.github.com>
2022-02-15 21:49:39 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -26,7 +26,7 @@ void check_warning_tolerance_approching_low_temp(float temperature)
 	
    if(temperature>= MINIMUM_TEMPERATURE || temperature <=Temperature_approaching_low_limit )
   {
-    Language?display("LOW_temperature_WARNING\n"):display("NIEDRIGE_Temperatur_WARNUNG \n");
+    display("LOW_temperature_WARNING\n");
   }
   
 }
