db79661848ec9527f29972bf70dcf23cdc7d1cd9
PraveenaJayakumar <90901057+PraveenaJayakumar@users.noreply.github.com>
2022-02-15 20:16:13 +0530

    Update Parameters.h

--- Parameters.h
+++ Parameters.h
@@ -4,3 +4,7 @@
 #define MAXIMUM_SOC    80.0
 #define MINIMUM_CHARGERATE    0
 #define MAXIMUM_CHARGERATE    0.8
+
+
+#define Discharge_Approach_limit MINIMUM_SOC + ( 0.05* MINIMUM_SOC)
+#define Charge_Peak_limit MAXIMUM_SOC - ( 0.05* MAXIMUM_SOC)
