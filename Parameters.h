ba9ebb0d1a76dba6b6c9147f08be7e4ec0562404
PraveenaJayakumar <90901057+PraveenaJayakumar@users.noreply.github.com>
2022-02-15 21:30:14 +0530

    Update Parameters.h

--- Parameters.h
+++ Parameters.h
@@ -12,5 +12,5 @@
 #define Temperature_approaching_low_limit (MINIMUM_TEMPERATURE + ( 0.05* MINIMUM_TEMPERATURE))
 #define Temperature_approaching_high_limit (MAXIMUM_TEMPERATURE - ( 0.05* MAXIMUM_TEMPERATURE))
 
-#define CHARGERATE_approaching_low_limit (MINIMUM_CHARGERATE + ( 0.05* MINIMUM_CHARGERATE))
-#define CHARGERATE_approaching_high_limit (MAXIMUM_CHARGERATE - ( 0.05* MAXIMUM_CHARGERATE))
+#define Chargerate_approaching_low_limit (MINIMUM_CHARGERATE + ( 0.05* MINIMUM_CHARGERATE))
+#define Chargerate_approaching_high_limit (MAXIMUM_CHARGERATE - ( 0.05* MAXIMUM_CHARGERATE))
